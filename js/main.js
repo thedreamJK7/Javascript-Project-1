2645b34d754fda7e00e4d95e88dbf13f2cf2d677
thedreamJK7 <jkubaev7@gmail.com>
2021-11-05 02:37:56 +0500

    Done

--- js/main.js
+++ js/main.js
@@ -1,8 +1,23 @@
 const quoties = document.getElementById('quoties');
 const quotiesauthorName = document.getElementById('author-name');
 const btn = document.getElementById('btn');
+const loading = document.getElementById('loader');
+const quoteContainer = document.getElementById('quote-container');
+
+function loader() {
+    loading.hidden = false;
+    quoteContainer.hidden = true;
+}
+
+function complete() {
+    if(!loading.hidden) {
+        quoteContainer.hidden = false;
+        loading.hidden = true;
+    }
+}
 
 async function getQuote() {
+    loader();
     const proxyUrl = 'https://whispering-tor-04671.herokuapp.com/';
     const ApiUrl = 'http://api.forismatic.com/api/1.0/?method=getQuote&lang=en&format=json';
     try {
@@ -19,9 +34,11 @@ async function getQuote() {
             quoties.classList.remove('quote-long');
         }
         quotiesauthorName.innerHTML = data.quoteAuthor;
+        complete();
     } catch(error) {
         getQuote();
     }
+
 }
 
 btn.addEventListener('click', getQuote);
